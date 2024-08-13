196d4d5fe85269d8592597cadd387d1175b68ff2
unknown <dasha.nebogina@mail.ru>
2024-08-13 21:52:28 +0300

    feat: if for hundreds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
+    if ((number.toString()).length == 3) {  //897
+        hundreds = Math.floor(number / 100); //8
+        number = number % 100; //97
+        tens = Math.floor(x / 10); //9
+        ones = number % 10; //7
+    }
+
+    return 0;
 }
