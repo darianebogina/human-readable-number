109ef59489c96ef7d9660a6df19c8adc3cf21761
unknown <dasha.nebogina@mail.ru>
2024-08-13 22:27:14 +0300

    feat: add str hundreds

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 module.exports = function toReadable(number) {
-    //897
+    let array = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
     let hundreds = Math.floor(number / 100); //8
     number = number % 100; //97
 
     let tens = Math.floor(number / 10); //9
     let ones = number % 10; //7
 
+    let str_hundred = array[hundreds] + ' hundred';
+    if (hundreds == 0) {
+        str_hundred = array[hundreds];
+    }
+    console.log(str_hundred);
     return 0;
 }
