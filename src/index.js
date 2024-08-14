7e8e6550c84a2c1dc0e26720c27bf50ee621002c
unknown <dasha.nebogina@mail.ru>
2024-08-14 22:15:35 +0300

    feat: almost done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable(number) {
     let arr_ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let arr_tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    let arr_special_tens =['', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arr_special_tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
     let hundreds = Math.floor(number / 100);
     number = number % 100;
@@ -9,26 +9,29 @@ module.exports = function toReadable(number) {
     let tens = Math.floor(number / 10);
     let ones = number % 10;
 
-    let answer = '';
+    let answer = [];
+
+    if (hundreds != 0) {
+        let str_hundred = arr_ones[hundreds] + ' hundred';
+        answer.push(str_hundred);
+    }
+
+    if (tens == 1) {
+        answer.push(arr_special_tens[ones]);
+    }
+
+    else if (tens != 0) {
+        answer.push(arr_tens[tens]);
+        if (ones != 0) {
+            answer.push(arr_ones[ones]);
+        }
+    }
+
+    else if ((tens == 0) && (ones != 0)){
+        answer.push(arr_ones[ones]);
+    }
+
     
-    let str_hundred = arr_ones[hundreds] + ' hundred';
-    if (hundreds == 0) {
-        str_hundred = arr_ones[hundreds];
-    }
 
-    if (tens == 0) {
-        answer += arr_ones[ones];
-    }
-    else if (tens == 1) {
-        answer += arr_special_tens[ones];
-    }
-    else {
-        answer += arr_tens[tens] + arr_ones[ones] ;
-    }
-
-    let str_ten = arr_tens[tens];
-    let str_ones = arr_ones[ones];
-    
-
-    return 0;
+    return answer.join(' ');
 }
